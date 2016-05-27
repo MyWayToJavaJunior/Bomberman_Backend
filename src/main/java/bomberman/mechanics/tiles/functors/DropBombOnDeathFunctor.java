81206a74a648e58297c136335f37bb3f2071fee4
Paul Maminov <xobotun@ya.ru>
2016-05-27 10:01:13 +0300

    Fixed new bonus to be removed after pickup.

--- src/main/java/bomberman/mechanics/tiles/functors/DropBombOnDeathFunctor.java
+++ src/main/java/bomberman/mechanics/tiles/functors/DropBombOnDeathFunctor.java
@@ -2,6 +2,8 @@ package bomberman.mechanics.tiles.functors;
 
 import bomberman.mechanics.Bomberman;
 import bomberman.mechanics.World;
+import bomberman.mechanics.WorldEvent;
+import bomberman.mechanics.interfaces.EventType;
 
 public class DropBombOnDeathFunctor extends ActionTileAbstractFunctor {
     public DropBombOnDeathFunctor(World eventList) {
@@ -11,5 +13,6 @@ public class DropBombOnDeathFunctor extends ActionTileAbstractFunctor {
     @Override
     public void applyAction(Bomberman bomberman) {
         bomberman.makeDropBombOnDeath();
+        eventList.addWorldEvent(new WorldEvent(EventType.TILE_REMOVED, owner.getType(), owner.getID(), 0, 0, null));
     }
 }
